63bb23eb4b4b65d199bfd2406a8646098ffdcc5e
Henry Annan <mrannan07@gmail.com>
2024-09-24 08:53:05 +0100

    Task 3 finished

--- L3T3.java
+++ L3T3.java
@@ -6,25 +6,33 @@ public class L3T3 {
         System.out.println("Enter ISBN");
         String ISBN = input.nextLine();
         int sum = 0;
-        char providedCheckChar = ISBN.charAt(12);
-        int providedCheckInt = Character.getNumericValue(providedCheckChar);
 
-        for (int i = 0; i < 12; i++) {
-            int num = Character.getNumericValue(ISBN.charAt(i));
-            if (i % 2 == 0) {
-                sum += num;
-            } else {
-                sum += num * 3;
+        if (ISBN.length() == 13) {
+            char providedCheckChar = ISBN.charAt(12);
+            int providedCheckInt = Character.getNumericValue(providedCheckChar);
+
+            for (int i = 0; i < 12; i++) {
+                int num = Character.getNumericValue(ISBN.charAt(i));
+                if (i % 2 == 0) {
+                    sum += num;
+                } else {
+                    sum += num * 3;
+                }
             }
-        }
 
-        int calculatedCheck = (10 - (sum % 10)) % 10;
+            int calculatedCheck = (10 - (sum % 10)) ;
+            if (calculatedCheck == 10){
+                calculatedCheck = 0;
+            }
 
-        if (providedCheckInt == calculatedCheck) {
-            System.out.println("ISBN validated");
+            if (providedCheckInt == calculatedCheck) {
+                System.out.println("ISBN validated");
+            } else {
+                System.out.println("ISBN NOT validated");
+                System.out.println("Check digit should be: " + calculatedCheck + " Your check digit was: " + providedCheckInt);
+            }
         } else {
-            System.out.println("ISBN NOT validated");
-            System.out.println("Check num should be: " + calculatedCheck + " Your check num was: " + providedCheckInt);
+            System.out.println("Your ISBN is not of the correct length");
         }
     }
 }
